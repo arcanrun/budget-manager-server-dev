83bb2832501a57cd0a48e449e9931370fef3edc3
arcanrun <arcan.run@gmail.com>
2019-09-12 01:21:15 +0300

    fix

--- server/auth/check_origin.py
+++ server/auth/check_origin.py
@@ -1,7 +1,8 @@
 def is_allowed_origin(request):
     allowed_origin = ['https://localhost:3000',
-                      'https://arcanrun.github.io/budget-manager']
+                      'https://arcanrun.github.io']
     try:
+        print('________________', request.META['HTTP_ORIGIN'])
         if request.META['HTTP_ORIGIN'] in allowed_origin:
             return True
         else:
