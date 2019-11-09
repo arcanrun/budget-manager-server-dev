6bba99b666c4ba37a9af473f67b8e4386910a008
arcanrun <arcan.run@gmail.com>
2019-11-09 03:42:22 +0300

    fix calendar

--- server/calculations/set_new_pay_date.py
+++ server/calculations/set_new_pay_date.py
@@ -10,6 +10,7 @@ import json
 import datetime
 from django.http import JsonResponse
 from ..models import Vkuser, History
+from datetime import timedelta
 
 from ..helpers import logger, get_updated_data, make_calculations, make_calculations_full,  costsPattern, history_saver, next_pay_day, get_id_from_vk_params, is_user_registered
 
@@ -25,17 +26,30 @@ def set_new_pay_date(request):
     vk_id = get_id_from_vk_params(str(req['params']))
     query_params = make_dict_from_query(str(req['params']))
     client_secret = insert_client_sign()
-    if is_valid(query=query_params, secret=client_secret) and (pay_day >= to_day or next_pay_day(pay_day) == next_pay_day(to_day)):
+    if is_valid(query=query_params, secret=client_secret):
 
         all_users = Vkuser.objects.all()
         to_day = datetime.datetime.now()
+
         for field in all_users:
             if (vk_id == field.id_vk):
-                formated_pay_day = datetime.datetime.strptime(
-                    pay_day[:pay_day.find('T')], '%Y-%m-%d')
-                new_days_to_payday = formated_pay_day - to_day
+                to_day_with_timezone = to_day + timedelta(hours=field.timezone)
 
+                pay_day = datetime.datetime.strptime(
+                    pay_day, "%Y-%m-%dT%H:%M:%S.%fZ") + timedelta(hours=field.timezone)
+
+                to_day_with_timezone = datetime.date.strftime(
+                    to_day_with_timezone, '%Y-%m-%d')
+
+                to_day_with_timezone = datetime.datetime.strptime(
+                    to_day_with_timezone, '%Y-%m-%d')
+
+                new_days_to_payday = pay_day - to_day_with_timezone
                 new_days_to_payday = new_days_to_payday.days
+                if(new_days_to_payday < 0):
+                    return JsonResponse({"RESPONSE": "BAD_REQUEST"})
+
+                print('3)', new_days_to_payday)
 
                 resArr = make_calculations_full(
                     field.common, field.fun, field.invest, new_days_to_payday,  field.budget)
