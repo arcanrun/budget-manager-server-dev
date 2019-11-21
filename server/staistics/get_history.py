589901dca6aee0d9a69a048740bfce60b7cfb43d
arcanrun <arcan.run@gmail.com>
2019-11-22 01:13:04 +0300

    is_ful_history

--- server/staistics/get_history.py
+++ server/staistics/get_history.py
@@ -27,38 +27,81 @@ def get_history(request):
         history_object = {}
         cost_object = {'type_cost': '', 'operation': '', 'value': '', 'id': ''}
         timezone = ''
+        is_full_history = True
 
         for user_field in user:
             if (vk_id == user_field.id_vk):
                 timezone = user_field.timezone
+                is_full_history = user_field.is_full_history
                 break
         for field in history:
+
             if (vk_id == field.id_vk):
-                with_time_zone = datetime.datetime.strptime(
-                    field.date, '%Y-%m-%d %H:%M:%S.%f') + timedelta(hours=timezone)
-                foramated_date = with_time_zone.strftime('%Y-%m-%d')
-                # foramated_date = datetime.datetime.strptime(
-                #     foramated_date, '%Y-%m-%d')
-                # foramated_date = foramated_date.strftime('%d.%m.%Y')
-                if foramated_date in history_object:
-                    cost_object['type_cost'] = field.type_costs
-                    cost_object['value'] = field.value
-                    cost_object['operation'] = field.operation
-                    cost_object['id'] = field.id
-                    cost_object['comment'] = field.comment
+                if not is_full_history:
+                    with_time_zone = datetime.datetime.strptime(
+                        field.date, '%Y-%m-%d %H:%M:%S.%f') + timedelta(hours=timezone)
+                    foramated_date = with_time_zone.strftime('%Y-%m-%d')
+                    # foramated_date = datetime.datetime.strptime(
+                    #     foramated_date, '%Y-%m-%d')
+                    # foramated_date = foramated_date.strftime('%d.%m.%Y')
+
+                    # if date presents in history_object:
+                    curent_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+                    current_date_timezone = datetime.datetime.strptime(
+                        curent_date, '%Y-%m-%d %H:%M:%S.%f') + timedelta(hours=timezone)
+                    current_date_formated = current_date_timezone.strftime(
+                        '%Y-%m')
+                    foramated_date_month = with_time_zone.strftime('%Y-%m')
+
+                    if current_date_formated == foramated_date_month:
+                        if foramated_date in history_object:
+                            cost_object['type_cost'] = field.type_costs
+                            cost_object['value'] = field.value
+                            cost_object['operation'] = field.operation
+                            cost_object['id'] = field.id
+                            cost_object['comment'] = field.comment
+
+                        # if date doesn't present in history_object then make new one:
+                        else:
+                            history_object[foramated_date] = []
+
+                            cost_object['type_cost'] = field.type_costs
+                            cost_object['value'] = field.value
+                            cost_object['operation'] = field.operation
+                            cost_object['id'] = field.id
+                            cost_object['comment'] = field.comment
+
+                        history_object[foramated_date].append(cost_object)
+
+                        cost_object = {'type_cost': '',
+                                       'operation': '', 'value': '', 'id': ''}
+
                 else:
-                    history_object[foramated_date] = []
+                    with_time_zone = datetime.datetime.strptime(
+                        field.date, '%Y-%m-%d %H:%M:%S.%f') + timedelta(hours=timezone)
+                    foramated_date = with_time_zone.strftime('%Y-%m-%d')
 
-                    cost_object['type_cost'] = field.type_costs
-                    cost_object['value'] = field.value
-                    cost_object['operation'] = field.operation
-                    cost_object['id'] = field.id
-                    cost_object['comment'] = field.comment
+                    if foramated_date in history_object:
+                        cost_object['type_cost'] = field.type_costs
+                        cost_object['value'] = field.value
+                        cost_object['operation'] = field.operation
+                        cost_object['id'] = field.id
+                        cost_object['comment'] = field.comment
 
-                history_object[foramated_date].append(cost_object)
+                    # if date doesn't present in history_object then make new one:
+                    else:
+                        history_object[foramated_date] = []
 
-                cost_object = {'type_cost': '',
-                               'operation': '', 'value': '', 'id': ''}
+                        cost_object['type_cost'] = field.type_costs
+                        cost_object['value'] = field.value
+                        cost_object['operation'] = field.operation
+                        cost_object['id'] = field.id
+                        cost_object['comment'] = field.comment
+
+                    history_object[foramated_date].append(cost_object)
+
+                    cost_object = {'type_cost': '',
+                                   'operation': '', 'value': '', 'id': ''}
 
         for k, v in history_object.items():
             v.reverse()
