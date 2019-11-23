af77fcc8966d7ea3e83a5d03623c70357b619dbe
arcanrun <arcan.run@gmail.com>
2019-11-24 01:11:34 +0300

    history month clear

--- server/profile/profile_settings.py
+++ server/profile/profile_settings.py
@@ -1,6 +1,7 @@
 import json
 from django.http import JsonResponse
 import datetime
+from datetime import timedelta
 from ..models import Vkuser, History
 
 from ..helpers import logger, get_updated_data, make_calculations, make_calculations_full,  costsPattern, history_saver, next_pay_day, get_id_from_vk_params, is_user_registered
@@ -50,13 +51,38 @@ def profile_settings(request):
                         'PAYLOAD': not field.is_full_history}
         logger('profile_manage:RESPONSE-->', response)
 
-        # if operation == 'history_delete_exact_month':
-        #     for field in history:
-        #         if (vk_id == field.id_vk and month == ???):
-        #             History.objects.filter(id_vk=vk_id).delete()
-        #     response = {'RESPONSE': 'HISTORY_DELETE_ALL',
-        #                 'PAYLOAD': vk_id}
-        # logger('profile_manage:RESPONSE-->', response)
+        if operation == 'history_delete_month':
+            req_month = str(req['payload'])
+            response = {'RESPONSE': 'ERROR_AUTH', 'PAYLOAD': 'ERROR'}
+            try:
+                foramated_date = ''
+                for field in all_users:
+                    if(vk_id == field.id_vk):
+                        timezone = field.timezone
+                        with_time_zone = datetime.datetime.strptime(
+                            req_month, '%Y-%m-%dT%H:%M:%S.%fZ') + timedelta(hours=timezone)
+                        foramated_date = with_time_zone.strftime('%m')
+                        break
+                for field in history:
+                    if(vk_id == field.id_vk):
+                        date = datetime.datetime.strptime(
+                            field.date, '%Y-%m-%d %H:%M:%S.%f')
+                        date = date.strftime('%m')
+                        if date == foramated_date:
+                            print('------------>', field.id)
+                            History.objects.filter(id=field.id).delete()
+
+                response = {'RESPONSE': 'MONTH_CLEARD', 'PAYLOAD': 'SUCCESS'}
+
+            except Exception:
+                response = {'RESPONSE': 'ERROR_AUTH', 'PAYLOAD': 'ERROR'}
+
+            # for field in history:
+            #     if (vk_id == field.id_vk and month == ???):
+            #         History.objects.filter(id_vk=vk_id).delete()
+            # response = {'RESPONSE': 'HISTORY_DELETE_ALL',
+            #             'PAYLOAD': vk_id}
+        logger('profile_manage:RESPONSE-->', response)
 
         return JsonResponse(response)
     else:
